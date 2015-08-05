729f6dcd93687c38ca2ef1312670d8019d16a5a2
Kamerynn Harrah <kamharrah8@gmail.com>
2015-08-05 19:10:30 -0400

    Add skeleton Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+  });
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('default', [
+
+  ]);
+};
