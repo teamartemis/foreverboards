3b0deab798ffc9febd6a23cdbceca475982c7a75
Wesley Tsai <wezley.tsai@gmail.com>
2015-08-06 20:40:32 -0700

    (feat) Added menu controller and template and board services skeleton

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -20,4 +20,31 @@ angular.module('artemis.services', [])
 
   };
 
+  return {
+    signin: signin,
+    signup: signup,
+    isAuth: isAuth,
+    signout: signout
+  };
+})
+.factory('Boards', function($http) {
+  var getBoards = function(user) {
+    // user = { username, password }
+    // get boards that are owned/administered by the user
+  };
+
+  var createBoard = function(data) {
+    // create board
+  };
+
+  var checkAccess = function(user, board) {
+    // return a promise that resolves into a boolean
+    // indicating whether or not the user has access to the board
+  };
+
+  return {
+    getBoards: getBoards,
+    createBoard: createBoard,
+    checkAccess: checkAccess
+  };
 });
