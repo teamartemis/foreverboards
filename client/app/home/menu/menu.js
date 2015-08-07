f2a70324d44a7983186a01fa7024f892e6f1ce24
Wesley Tsai <wezley.tsai@gmail.com>
2015-08-07 15:30:29 -0700

    (feat) Added a home state, parent of menu and board state. Reorganized folders/files.

--- client/app/home/menu/menu.js
+++ client/app/home/menu/menu.js
@@ -5,21 +5,24 @@ angular.module('artemis.menu', [
   // --- testing
   $scope.boards = [
     {
+      id: 1,
       deceasedName: 'Ned Stark',
       url: 'http://www.google.com'
     },
     {
+      id: 2,
       deceasedName: 'Tywin Lannister',
       url: 'http://www.amazon.com'
     },
     {
+      id: 3,
       deceasedName: 'The Hound',
       url: 'http://www.apple.com'
     }
   ];
 
   $scope.create = function() {
-    $state.go('menu.create');
+    $state.go('home.menu.create');
   };
 
   $scope.getBoards = function() {
