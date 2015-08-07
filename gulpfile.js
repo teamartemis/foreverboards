0c4def261e344ec5f1a980315d49e4d013209e53
David Craven <david@craven.ch>
2015-08-06 22:30:02 -0700

    (build) Add template build task Disabled name mangling because it is preventing angular's dependency injection from working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var bower = require('gulp-bower');
 var domSrc = require('gulp-dom-src');
 var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
@@ -7,7 +8,20 @@ var minifyHTML = require('gulp-minify-html');
 var cheerio = require('gulp-cheerio');
 var nodemon = require('gulp-nodemon');
 var eslint = require('gulp-eslint');
+var angularTemplates = require('gulp-angular-templates');
 var pjson = require('./package.json');
+
+gulp.task('bower', function() {
+  return bower({ cmd: 'update'});
+});
+
+gulp.task('build:templates', function() {
+  return gulp.src('client/app/**/*.html')
+    .pipe(angularTemplates({module: 'artemis', basePath: 'app/'}))
+    .pipe(concat('app.templates.min.js'))
+    .pipe(uglify({mangle: false}))
+    .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('build:css', function() {
   return domSrc({file:'client/index.html', selector:'link', attribute:'href'})
@@ -19,7 +33,7 @@ gulp.task('build:css', function() {
 gulp.task('build:js', function() {
   return domSrc({file:'client/index.html', selector:'script', attribute:'src'})
     .pipe(concat('app.full.min.js'))
-    .pipe(uglify())
+    .pipe(uglify({mangle: false}))
     .pipe(gulp.dest('dist/'));
 });
 
@@ -29,13 +43,18 @@ gulp.task('build:indexHtml', function() {
       $('script').remove();
       $('link').remove();
       $('body').append('<script src="app.full.min.js"></script>');
+      $('body').append('<script src="app.templates.min.js"></script>');
       $('head').append('<link rel="stylesheet" href="app.full.min.css">');
     }))
     .pipe(minifyHTML())
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build', ['build:css', 'build:js', 'build:indexHtml']);
+gulp.task('build:all', ['build:css', 'build:js', 'build:templates', 'build:indexHtml']);
+
+gulp.task('build', ['bower'], function() {
+  return gulp.start('build:all');
+});
 
 gulp.task('lint', function() {
   return gulp.src(['*.js', 'client/**/*.js', '!client/bower_components/**/*.js', 'server/**/*.js'])
@@ -49,8 +68,7 @@ gulp.task('test', ['lint']);
 gulp.task('watch', function() {
   nodemon({
     script: pjson.main,
-    ext: 'js html',
-    env: { 'NODE_ENV': 'development' }
+    ext: 'js html'
   });
 });
 
