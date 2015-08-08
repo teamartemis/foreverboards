a4b54002eeeb6bf0142d3fe986761941e6c1dae6
Wesley Tsai <wezley.tsai@gmail.com>
2015-08-07 19:35:06 -0700

    (feat) Add posts to board as directive, minor styles for posts

--- client/app/home/board/board.js
+++ client/app/home/board/board.js
@@ -20,4 +20,21 @@ angular.module('artemis.board', [
 
   $scope.getBoard();
   $scope.getPosts();
+})
+.directive('post', function() {
+  return {
+    restrict: 'EA', // can be used as Element or Attribute
+    templateUrl: 'app/home/board/post.html',
+    replace: true, // replaces the directive tags in the DOM
+    scope: { // defining object literal -> isolate scope (does not delegate upwards to parents)
+      source: '='
+      // '=' -> 2-way binding to whatever I designate as 'source', can be object/array
+      // '@' means 1-way binding (changes in directive do not change outside)
+      // '&' means passing in a function
+    },
+    link: function(scope, ele, attr) {
+      // do stuff here
+      console.log('post');
+    }
+  };
 });
