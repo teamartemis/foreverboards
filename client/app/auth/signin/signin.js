ab0871946a2e3f5fed39c32d394b2ae476dfa546
David Craven <david@craven.ch>
2015-08-07 20:21:36 -0700

    (feat) Redirect to menu on login

--- client/app/auth/signin/signin.js
+++ client/app/auth/signin/signin.js
@@ -1,13 +1,13 @@
 angular.module('artemis.auth.signin', [])
 
-.controller('SignInController', function($scope, $window, Users) {
+.controller('SignInController', function($scope, $window, $state, Users) {
   $scope.signin = function() {
     Users.signin({
       username: $scope.username,
       password: $scope.password
     })
     .then(function() {
-      console.log('Promise received.');
+      $state.go('home.menu');
     })
     .catch(function(err) {
       console.error(err);
