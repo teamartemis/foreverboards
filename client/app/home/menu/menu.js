79ce39e6b1d317e9e0dfa333eba3b567daaf8bdb
David Craven <david@craven.ch>
2015-08-07 19:34:45 -0700

    (feat) Menu controller displays boards

--- client/app/home/menu/menu.js
+++ client/app/home/menu/menu.js
@@ -2,32 +2,15 @@ angular.module('artemis.menu', [
   'artemis.menu.create'
 ])
 .controller('MenuController', function($scope, $state, Boards) {
-  // --- testing
-  $scope.boards = [
-    {
-      id: 1,
-      deceasedName: 'Ned Stark',
-      url: 'http://www.google.com'
-    },
-    {
-      id: 2,
-      deceasedName: 'Tywin Lannister',
-      url: 'http://www.amazon.com'
-    },
-    {
-      id: 3,
-      deceasedName: 'The Hound',
-      url: 'http://www.apple.com'
-    }
-  ];
+  $scope.boards = [];
 
   $scope.create = function() {
     $state.go('home.menu.create');
   };
 
   $scope.getBoards = function() {
-    Boards.getBoards().then(function(response) {
-      $scope.boards = response.data; // TODO: conform to Parse API
+    Boards.getBoards().then(function(res) {
+      $scope.boards = res.data.results;
     });
   };
 
@@ -35,6 +18,24 @@ angular.module('artemis.menu', [
     return $state.current.name === name;
   };
 
-  // initialize board
-  // $scope.getBoards();
+  $scope.getBoards();
 });
+
+// --- testing
+/*
+  {
+    objectId: 1,
+    deceasedName: 'Ned Stark',
+    url: 'http://www.google.com'
+  },
+  {
+    objectId: 2,
+    deceasedName: 'Tywin Lannister',
+    url: 'http://www.amazon.com'
+  },
+  {
+    objectId: 3,
+    deceasedName: 'The Hound',
+    url: 'http://www.apple.com'
+  }
+*/
