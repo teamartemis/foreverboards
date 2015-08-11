4ed011e84fe9ab0bfbf90bebfe8159a6b70d667a
David Craven <david@craven.ch>
2015-08-10 20:41:50 -0700

    (cleanup) Remove unnecessary cors middleware

--- server/server-config.js
+++ server/server-config.js
@@ -1,9 +1,6 @@
 var express = require('express');
-var cors = require('cors');
 var upload = require('./upload');
 var app = express();
-
-app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('dist'));
